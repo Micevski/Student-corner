32c7231d83e9d3383dd222cdf43e4866273ce3de
Atanas <atenikolovski@gmail.com>
2019-02-14 22:04:21 +0100

    Components for question added

---
+++ sc-client/src/components/QuestionOpened.vue
@@ -0,0 +1,32 @@
+<template>
+    <div id="question">
+        <h1 >{{question.title}}</h1>
+        <p>{{question.question}}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+                question:{}
+            }
+        },
+        created () {
+            this.getQuestionById();
+        },
+        methods: {
+            getQuestionById:function () {
+                this.$http.get("/api/user/question/"+this.$route.params.id)
+                    .then((res)=>{
+                        console.log(res)
+                        this.question = res.data
+                    }).catch((e)=> console.log(e))
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ sc-client/src/components/Questions.vue
@@ -0,0 +1,40 @@
+<template>
+    <div id="questions" v-for="question in questions">
+        <a :href="'/#/question/'+question.id">
+            <v-card-title class="v-card__title--primary">
+                <div>
+                    <h3 class="headline mb-0">{{question.title}}</h3>
+                </div>
+            </v-card-title>
+            <div>
+                <p>{{question.question.substring(0,30)}}</p>
+            </div>
+        </a>
+    </div>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+                questions:{}
+            }
+        },
+        created () {
+            this.getQuestions();
+        },
+        methods: {
+            getQuestions:function () {
+                this.$http.get("/api/user/question")
+                    .then((res)=>{
+                        console.log(res)
+                        this.questions = res.data
+                    }).catch((e)=> console.log(e))
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
